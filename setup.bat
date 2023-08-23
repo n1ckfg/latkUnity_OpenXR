fc0e12067346cb877e4847156e6c908106d0e2ec
Nick Fox-Gieg <nick@fox-gieg.com>
2023-08-23 09:19:36 -0400

    shell

--- setup.bat
+++ setup.bat
@@ -1,11 +1,15 @@
 @echo off
 
+git pull
+
 git submodule init
 git submodule update --init --recursive
 git submodule sync
-git submodule foreach git checkout master
+git submodule foreach git checkout master > nul 2> nul
+git submodule foreach git checkout main > nul 2> nul
 git submodule foreach git reset --hard
-git submodule foreach git pull origin master
+git submodule foreach git pull origin master > nul 2> nul
+git submodule foreach git pull origin main > nul 2> nul
 
 rem cd Assets\latkxGDrive
 rem git checkout unity-2018-lts
--- setup.sh
+++ setup.sh
@@ -10,13 +10,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 cd $DIR
 
+git pull
+
 git submodule init
 git submodule update --init --recursive
 git submodule sync
-git submodule foreach git checkout master
+git submodule foreach git checkout master 2>/dev/null
+git submodule foreach git checkout main 2>/dev/null
 git submodule foreach git reset --hard
-git submodule foreach git pull origin master
-
-#cd Assets/latkxGDrive
-#git checkout unity-2018-lts
-#cd $DIR
+git submodule foreach git pull origin master 2>/dev/null
+git submodule foreach git pull origin main 2>/dev/null
